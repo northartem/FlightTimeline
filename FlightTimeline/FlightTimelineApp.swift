6d0a592a5dbd1bcd2c368ea549a8bc00e6706f1c
AssanCred <temix@live.ru>
2023-06-19 16:13:44 +0700

    Initial Commit

---
+++ FlightTimeline/FlightTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimelineApp.swift
+//  FlightTimeline
+//
+//  Created by Артём Латушкин on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
