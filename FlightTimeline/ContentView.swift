ba4bf6ae1d82299921194198655157bdb524939a
AssanCred <temix@live.ru>
2023-06-22 21:11:03 +0700

    change main view

--- FlightTimeline/ContentView.swift
+++ FlightTimeline/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            HomeScreenView()
+                .tabItem {
+                    Image(systemName: "airplane")
+                    Text("Home")
+                }
+            UserProfileView()
+                .tabItem {
+                    Image(systemName: "person.circle")
+                    Text("Profile")
+                }
         }
-        .padding()
     }
 }
 
